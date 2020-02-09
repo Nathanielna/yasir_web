c2d0af387eef4c58f38d25e0916c8b5cd5b615af
Yan <1832654908@qq.com>
2020-02-09 19:31:24 +0800

    新增错误页面处理 404等 路由设置改为自动路由

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	_ "yasir_web/routers"
-	_ "yasir_web/sysinit"
+	//_ "yasir_web/sysinit"  //使用数据库请取消注释
+	"github.com/astaxie/beego/logs"
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	// 日志输出到log目录下
+	logs.Async()
+	logs.SetLogger(logs.AdapterMultiFile, `{"filename":"log/test.log","separate":["emergency", "alert", "critical", "error", "warning", "notice", "info", "debug"]}`)
 	beego.Run()
 }
 
--- routers/router.go
+++ routers/router.go
@@ -6,5 +6,11 @@ import (
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	// ERROR 页面  404/403/401/500/503
+	beego.ErrorController(&controllers.ErrorController{})
+	//自动匹配路由
+	//beego.AutoRouter(&controllers.ObjectController{})
+	//object/login   调用 ObjectController 中的 Login 方法
+    //object/logout  调用 ObjectController 中的 Logout 方法
+	beego.AutoRouter(&controllers.MainController{})
 }
